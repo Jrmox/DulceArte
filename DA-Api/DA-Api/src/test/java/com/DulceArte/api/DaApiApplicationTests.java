587f52ee70f9d347071f0a0674a411b7a4fc7005
jmonterrosa <jmonterrosa@xolosa.com>
2025-11-24 19:04:47 -0600

    Primera version del api de DU

---
+++ DA-Api/DA-Api/src/test/java/com/DulceArte/api/DaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DulceArte.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
