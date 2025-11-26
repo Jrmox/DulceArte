88147e24b192b524eee68204d2c7ebddcce33809
jmonterrosa <jmonterrosa@xolosa.com>
2025-11-25 20:28:11 -0600

    limpieza del proyecto inicial

---
+++ dulcearte-backend/src/test/java/com/dulcearte/api/DulceArteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dulcearte.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DulceArteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
